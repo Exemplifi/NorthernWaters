122d6392ed0840017ffe0522099a88b6709fafeb
subhasishnath45 <subhasishnath45@gmail.com>
2025-06-13 15:08:08 +0530

    Frontend Setup fixes as per suggestions and reference taken from JTAFLA project.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
-import '../assets/scss/main.scss';
+import $ from "jquery";
+import "slick-carousel";
+
+// Make jQuery globally available
+window.$ = window.jQuery = $;
+
+import "../assets/scss/styles.scss";
+import * as bootstrap from "bootstrap";
+
+import { createIcons, icons } from "lucide";
+
+// import './custom.js';
+
+createIcons({ icons });
+
 
 
